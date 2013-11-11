7a6949f2d0527a6ff7d558f0b8947b16b4c5c628
Ivar Vong <ivar@ivarvong.com>
2013-11-10 20:44:27 -0800

    back and forth. should i be writing a lighbox?

--- app.js
+++ app.js
@@ -17,25 +17,44 @@ window.bootstrap_data = function(data) {
 }
 $(document).ready(function() {
 
+	window.light_box_active = false;
 	var source   = $("#entry-template").html();
 	var template = Handlebars.compile(source);
 	var $thumbnails = $('.thumbnails');
 	
+	var show_lightbox = function(contents) {
+		console.log('show_lightbox');
+		window.light_box_active = true;
+		$('.lightbox-wrapper').css('display', 'block');
+		$('.lightbox').html(contents).css('display', 'block');
+	}
+	var hide_lightbox = function() {
+		console.log('hide_lightbox');
+		$('.lightbox-wrapper').css('display', 'none');
+		$('.lightbox').html('');	
+		window.light_box_active = false;
+	}
+
 	var build_thumbnails = function(data) {		
 		data.forEach(function(item){
 			$thumbnails.append( template(item) );
 		});
-		$('.photo').on('click', function(el) {
+		$('.photo').on('click', function(el) {			
 			$(this).find('.inner').css('display', 'block');
 			var img_tag = "<img src='" + $(this).find('.big-img').attr('data-src') + "' />";
-			//console.log(img_tag);
-			$('.lightbox').html(img_tag).css('display', 'block');
+			console.log(img_tag);			
+			show_lightbox(img_tag);
+		});
+		$('.lightbox-wrapper').on('click', function(el) {
+			if (window.light_box_active === true) {
+				hide_lightbox();
+			}
 		});
 
 	};
 
 	if (window.data == undefined) {
-		alert('uh oh')
+		alert('Something terrible is happening. I\'m sorry');
 	} else {
 		build_thumbnails(window.data);
 	}
