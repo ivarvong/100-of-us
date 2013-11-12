861da618820f5940f9b07c6b2842da6a4ea7aed4
Bmai <Bmai93@gmail.com>
2013-11-11 23:18:57 -0800

    Revert "update data"
    This reverts commit 13717abdb38ec02d8f7a92fa447dd5196a50675a.

--- pull_data.rb
+++ pull_data.rb
@@ -9,7 +9,7 @@ records = []
 
 # need to get this from ASCII-8BIT to UTF-8 otherwise it'll explode
 # http://stackoverflow.com/questions/4867883/convert-non-ascii-chars-from-ascii-8bit-to-utf-8
-data = open(url).read.force_encoding('utf-8')
+data = open(url).read.force_encoding('utf-8').encode
 
 CSV.parse(data, headers: :first_row) do |row|  
   	records << {
